c3e7026ff5b661212cc2856a91d984dc558b3850
hbandesh <hbandesh@sapient.com>
2018-07-15 11:22:13 +0530

    preloading images

--- app/components/head.js
+++ app/components/head.js
@@ -7,6 +7,9 @@ export class Head extends React.Component {
                 <title>CHUNKED POC</title>
                 <link rel="stylesheet" type="text/css" href="/style.css"/>
                 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossOrigin="anonymous"/>
+                <link rel="dns-prefetch" href="www.hdwallpaper.nu"/>
+                <link rel="preconnect" href="https://www.hdwallpaper.nu"/>
+                <link rel="preload" href="https://www.hdwallpaper.nu/wp-content/uploads/2017/07/thor-1.jpg" as="image"/>
             </head>
         );
     }
--- app/components/mainContent.js
+++ app/components/mainContent.js
@@ -9,28 +9,31 @@ export class MainContent extends React.Component {
 
     render() {
         return(
-            <table className="table">
-                <thead className="thead-dark">
-                    <tr>
-                        <th>ID</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Avatar</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.props.data.map((itm,idx) => {
-                        return(
-                            <tr key={itm.first_name}>
-                                <td key={itm.id}>{itm.id}</td>
-                                <td key={itm.first_name}>{itm.first_name}</td>
-                                <td key={itm.last_name}>{itm.last_name}</td>
-                                <td key={itm.avatar}><img src={itm.avatar}/></td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
+            <React.Fragment>
+                <table className="table">
+                    <thead className="thead-dark">
+                        <tr>
+                            <th>ID</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Avatar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.data.map((itm,idx) => {
+                            return(
+                                <tr key={itm.first_name}>
+                                    <td key={itm.id}>{itm.id}</td>
+                                    <td key={itm.first_name}>{itm.first_name}</td>
+                                    <td key={itm.last_name}>{itm.last_name}</td>
+                                    <td key={itm.avatar}><img src={itm.avatar}/></td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+                <img src="https://www.hdwallpaper.nu/wp-content/uploads/2017/07/thor-1.jpg" alt="Mighty Thor!!!!"/>
+            </React.Fragment>
         );
     }
 }
